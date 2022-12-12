47c9f65fac5625b1a13d8e98130a73367f822d7f
Matheus Pimenta <matheuspimenta@Matheuss-Laptop.local>
2022-12-12 10:52:38 -0300

    Updating text-breaker tool

--- text-breaker/index.js
+++ text-breaker/index.js
@@ -1,25 +1,32 @@
-// wirte a function that receives text: string, limit: number and count how many letter we'd have and break it into a new line after 80 without break any word
+function breakString(str, chars) {
+  // Create an empty result string
+  let result = '';
 
+  // Split the input string into an array of words
+  let words = str.split(' ');
 
-function emailFormatter(text, limit) {
-    if(typeof text === 'number' || typeof limit !== 'number') return "ERROR - Incorrect Input";
-    
-    if(text.length <= limit) return text.toString();
-    
-    let result = '';
-    const words = text.split(' ')
-    let line = ''
-  
-    for (let i = 0; i < words.length; i++) {
-      if(line.length + words[i].length <= limit) {
-        line += words[i] + ' ';
-      } else {
-        result += line.trim() + '\n'
-        line = words[i] + ' '
-      }
+  // Keep track of the current line length
+  let lineLength = 0;
+
+  // Loop through the array of words
+  for (let i = 0; i < words.length; i++) {
+    // If the current word will make the line too long,
+    // add a newline character to the result string
+    if (lineLength + words[i].length > chars) {
+      result += '\n';
+      lineLength = 0;
     }
-  
-    const finalText = result += line
-  
-    return finalText.trim()
+
+    // Add the current word to the result string, followed by a space
+    result += words[i] + ' ';
+
+    // Increase the line length by the length of the current word plus one (for the space)
+    lineLength += words[i].length + 1;
   }
+
+  // Return the result string
+  return result;
+}
+
+let str = 'This is a very long string that needs to be broken into multiple lines.';
+let brokenString = breakString(str, 40);
